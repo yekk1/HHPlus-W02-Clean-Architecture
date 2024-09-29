2f92eef3df6744e2012f369af01f2972d38a7bbb
yekk1 <daeyoi96@gmail.com>
2024-09-29 18:01:30 +0900

    Feat: first commit

---
+++ src/main/java/com/sparta/hhplusw02cleanarchitecture/HhPlusW02CleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.hhplusw02cleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhPlusW02CleanArchitectureApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HhPlusW02CleanArchitectureApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/hhplusw02cleanarchitecture/HhPlusW02CleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.hhplusw02cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhPlusW02CleanArchitectureApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
